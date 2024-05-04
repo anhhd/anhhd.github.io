2fd516d1c9cf888b4ebcce329c38a5c0f37dcf5a
Anh Hoang Duc <hoangducanh89@gmail.com>
2024-05-04 20:11:27 +0700

    Update website

---
+++ _2_copy_book_python.bat
@@ -0,0 +1,10 @@
+REM ------Python Book ------------------------
+
+REM - Create ds-book-r in public folder
+
+set "source=..\ds-book-python\_build\html"
+set "destination=.\ds-book-python\"
+
+del /S "%destination%\*" /F /Q
+
+xcopy /s "%source%" "%destination%"
